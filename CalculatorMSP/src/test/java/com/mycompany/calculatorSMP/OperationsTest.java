43800132d47718bbcedd88b65cbabeee137abe58
Johegvel <johegvel@espol.edu.ec>
2025-07-30 15:33:00 -0500

    Test

---
+++ CalculatorMSP/src/test/java/com/mycompany/calculatorSMP/OperationsTest.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.calculatorSMP;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CltControl
+ */
+public class OperationsTest {
+    
+    public OperationsTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "";
+        String result = Operations.MakeFormula();
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+    
+}
